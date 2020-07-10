c88e598e0b109232bda375d2345d8271c4db824b
Shad Decker <DeckerShadN@JohnDeere.com>
2020-07-10 14:43:45 -0500

    click event added; locating row and column

--- js/script.js
+++ js/script.js
@@ -27,12 +27,6 @@ const myGame = new Game();
 let gameBoardElement = document.querySelector("canvas");
 let gameBoardCanvas = gameBoardElement.getBoundingClientRect();
 
-//function to update the gameboard to the current clicked location
-function setPosition() {
-  myGame.locationX = event.clientX - gameBoardCanvas.left; // x == the location of the click in the document - the location (relative to the left) of the canvas in the document
-  myGame.locationY = event.clientY - gameBoardCanvas.top; // y == the location of the click in the document - the location (relative to the top) of the canvas in the document
-}
-
 //function to update each squares boundries
 function defineSquare(upperLeft, upperRight, lowerLeft, lowerRight) {
   tmpSquare = new Square(upperLeft, upperRight, lowerLeft, lowerRight);
@@ -43,14 +37,16 @@ function defineSquare(upperLeft, upperRight, lowerLeft, lowerRight) {
 function drawDots() {
   var ctx = gameBoardElement.getContext("2d");
   ctx.fillStyle = "#000000";
-  for (j = 1; j <= 5; j++) {
-    for (k = 1; k <= 5; k++) {
+  for (j = 1; j <= 6; j++) {
+    for (k = 1; k <= 6; k++) {
       locationX = (gameBoardCanvas.left + myGame.squareSize) * j;
       locationY = (gameBoardCanvas.top + myGame.squareSize) * k;
-      //draw the dot
-      ctx.beginPath();
-      ctx.arc(locationX, locationY, myGame.dotSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
-      ctx.fill(); // Close the path and fill.
+      if (j < 6 && k < 6) {
+        //draw the dot
+        ctx.beginPath();
+        ctx.arc(locationX, locationY, myGame.dotSize, 0, Math.PI * 2, true); // Draw a point using the arc function of the canvas with a point structure.
+        ctx.fill(); // Close the path and fill.
+      }
       //calculate and add new square dimensions to the class
       let upperLeft = [locationX - myGame.squareSize, locationY - myGame.squareSize];
       let upperRight = [locationX, locationY - myGame.squareSize];
@@ -61,10 +57,34 @@ function drawDots() {
   }
 }
 
+//function to handle the click event
+function clickHandler() {
+  setPosition();
+  isInABox();
+}
+
+//function to update the gameboard to the current clicked location
+function setPosition() {
+  myGame.locationX = event.clientX - gameBoardCanvas.left; // x == the location of the click in the document - the location (relative to the left) of the canvas in the document
+  myGame.locationY = event.clientY - gameBoardCanvas.top; // y == the location of the click in the document - the location (relative to the top) of the canvas in the document
+}
+//function to determine if they clicked a side of a square
+function isInABox() {
+  console.log(myGame.locationX, myGame.locationY);
+  for (j = 1; j <= 6; j++) {
+    if (myGame.locationX === (gameBoardCanvas.left + myGame.squareSize) * j) {
+      console.log("column " + j);
+    }
+    if (myGame.locationY === (gameBoardCanvas.top + myGame.squareSize) * j) {
+      console.log("row " + j);
+    }
+  }
+}
+
 //function to initialize the game
 function setUpBoard() {
   drawDots();
-  gameBoardElement.addEventListener("click", setPosition);
+  gameBoardElement.addEventListener("click", clickHandler);
 }
 
 setUpBoard();
